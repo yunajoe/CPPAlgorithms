94fc76121e16fde11a924c69ba85a7f3c9853edb
yunajoe <yunajoe@gmail.com>
2024-10-09 13:46:57 +0900

    basicDataType.cpp

---
+++ hackerrank/easy/basicDataType.cpp
@@ -0,0 +1,20 @@
+#include <cmath>
+#include <cstdio>
+using namespace std;
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
+    int a; 
+    long b; 
+    char c;
+    float d; 
+    double e;
+    scanf("%d %ld %c %f %lf", &a, &b, &c, &d, &e); 
+    printf("%d\n", a);
+    printf("%ld\n", b);
+    printf("%c\n", c); 
+    printf("%f\n", d); 
+    printf("%lf\n", e);    
+
+   
+    return 0;
+}
